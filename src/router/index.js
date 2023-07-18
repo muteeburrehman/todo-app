175e1158d6b7c0523297169eb23e93373ee75bd1
muteeburrehman <muteeb285@gmail.com>
2023-07-18 18:43:34 +0500

    Changes in Main App vue code

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import {auth} from "../../public/firebase";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/about',
@@ -14,7 +17,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      requiresAuth:true
+    }
+  },
+  {
+    path:'/login',
+    name: 'LoginComp',
+    component: () =>import('../views/LoginComp.vue')
   }
 ]
 
@@ -22,5 +33,16 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  if(to.path=== '/login && auth.currentUser'){
+    next('/')
+    return;
+  }
+  if(to.matched.some(record =>record.meta.requiresAuth)&& !auth.currentUser)
+  {
+    next('/login')
+    return;
+  }
+  next();
+})
 export default router
