99e13d661884c975aea0131e6d546e76cee4ccad
muteeburrehman <muteeb285@gmail.com>
2023-07-18 19:19:13 +0500

    Config with firebase and add js code of FB

---
+++ public/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD6cuGkGRKKYfh3XINmoOR2_akHhw_BsEo",
+    authDomain: "git-firebase-f1306.firebaseapp.com",
+    projectId: "git-firebase-f1306",
+    storageBucket: "git-firebase-f1306.appspot.com",
+    messagingSenderId: "722067411265",
+    appId: "1:722067411265:web:a0fa802dee004774ef51c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
